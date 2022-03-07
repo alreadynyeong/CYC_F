cf89b98243acac1154b79a9b6569c0283e555dc0
alreadynyeong <kjw87130@naver.com>
2022-03-07 19:49:16 +0900

    #2 closet 카테고리 선택 페이지 구현

--- cyc/src/component/closet.js
+++ cyc/src/component/closet.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from '../img/cyc_logo.png';
-import '../css/App.css';
+import '../css/closet.css';
 
 function Home() {
   return (
-    <div className="App">
-      closet page
-
+    <div className="Closet">
+        <div className='header'><img src={logo}  className="headimg"/></div>
+        <div className='cate'>
+            <div className='outer'>아우터</div><br></br><br></br>
+            <div className='top'>상의</div><br></br><br></br>
+            <div className='bottom'>하의</div><br></br><br></br>
+            <div className='onepiece'>원피스</div><br></br><br></br>
+            <div className='shoes'>신발</div><br></br><br></br>
+            <div className='acc'>악세서리</div>
+        </div>
     </div>
   );
 }
--- cyc/src/component/home.js
+++ cyc/src/component/home.js
@@ -9,9 +9,9 @@ function Home() {
     <div className="App">
       <div className='logo'><img src={logo}  className="logoimg"/></div>
       <div className='select'>
-        <div className='color'><Link to="color">Color</Link></div>
-        <div className='cody'><Link to="cody">Cody</Link></div>
-        <div className='closet'><Link to="closet">Closet</Link></div>
+        <div className='home_color'><Link to="color">Color</Link></div>
+        <div className='home_cody'><Link to="cody">Cody</Link></div>
+        <div className='home_closet'><Link to="closet">Closet</Link></div>
       </div>
     </div>
   );
