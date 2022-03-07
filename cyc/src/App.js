b75642ed156843b7cd1388a5984baea20f716891
alreadynyeong <kjw87130@naver.com>
2022-03-07 15:01:30 +0900

    #1 home page css적용/ 임시 글꼴 적용

--- cyc/src/App.js
+++ cyc/src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='logo'> <img src={logo} className="App-logo" alt="logo" /></div>
+      <div className='logo'><img src={logo}  className="logoimg"/></div>
       <div className='select'>
         <div className='color'>COLOR</div>
-        <div className='closet'>CLOSET</div>
         <div className='cody'>CODY</div>
+        <div className='closet'>CLOSET</div>
       </div>
     </div>
   );
