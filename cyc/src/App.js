a256dd16914473698b5cdd3f08b8a8d0542cd31b
alreadynyeong <kjw87130@naver.com>
2022-03-07 14:26:30 +0900

    #1 로고 이미지 삽입

--- cyc/src/App.js
+++ cyc/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './cyc_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo'> <img src={logo} className="App-logo" alt="logo" /></div>
+      <div className='select'>
+        <div className='color'>COLOR</div>
+        <div className='closet'>CLOSET</div>
+        <div className='cody'>CODY</div>
+      </div>
     </div>
   );
 }
